76958ac33b4aa9e40fe97b011fd29ba0fb25d494
Mateusz Budner <matbudn@mat.umk.pl>
2025-11-05 16:52:21 +0100

    Sprawdz czy wartosc jest ujemna
    Przejdź do gałęzi sprawdzanie. Do pliku program.cpp dodaj sprawdzanie, czy wczytana liczba jest nieujemna. W przypadku wczytania wartości ujemnej program ma kończyć działanie z wypisaniem stosownego komunikatu (nie ma być wypisywana żadna wartość liczbowa). Dodaj zmodyfikowany plik do przechowalni oraz utwórz nową migawkę zawierającą zmodyfikowany plik (można to zrobić albo pojedynczym poleceniem albo dwoma poleceniami - do wyboru).

--- program.cpp
+++ program.cpp
@@ -4,6 +4,12 @@ int main() {
 	int n;
 	std::cout << "Podaj liczbę: ";
 	std::cin >> n;
+
+	if (n < 0) {
+		std::cout << "Liczba jest ujemna." << std::endl;
+		return 1;
+	}
+
 	std::cout << "Liczba: " << n << std::endl;
 	return 0;
 }
