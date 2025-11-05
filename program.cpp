5bc480b1ba1100f8a29bcc624220dc7be02f168d
Mateusz Budner <matbudn@mat.umk.pl>
2025-11-05 16:30:25 +0100

    Dodaj fibonacci
    Przejdź do gałęzi obliczenia. Do pliku program.cpp dodaj funkcję fibonacci wartość zadanego elementu ciągu Fibonacciego oraz zmodyfikuj program tak, aby wypisywał na standardowe wyjście wczytaną liczbę oraz wartość elementu ciągu Fibonacciego o indeksie równym wczytanej liczbie. Dodaj zmodyfikowany plik do przechowalni oraz utwórz nową migawkę zawierającą zmodyfikowany plik (można to zrobić albo pojedynczym poleceniem albo dwoma poleceniami - do wyboru).

--- program.cpp
+++ program.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 
+unsigned int fibonacci(unsigned int n) {
+	if (n == 0 || n == 1) {
+		return n;
+	}
+
+	return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
 int main() {
-	int n;
+	unsigned int n;
 	std::cout << "Podaj liczbę: ";
 	std::cin >> n;
 	std::cout << "Liczba: " << n << std::endl;
+	std::cout << "Fibonacci: " << fibonacci(n) << std::endl;
 	return 0;
 }
